dcda88e84215bdae0cba295beea6b0420ab5eee7
Toby Batch <tobias@neontribe.co.uk>
2017-12-13 11:58:43 +0000

    Cleaned out old drupal stuff, added github issue branch creator

--- 3screen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP1 --off --output HDMI1 --off --output VGA-1-1 --off --output LVDS1 --mode 1600x900 --pos 3840x180 --rotate normal --output VGA2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DVI-2-0 --mode 1920x1080 --pos 0x0 --rotate normal
